6cfcc38432736b69f76cc7f6491072ec16d780a3
Liz <78451440+lizmery@users.noreply.github.com>
2021-09-16 12:52:34 -0700

    Update config.php
    Fix spelling error

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $conn = new mysqli('localhost', 'root', '', 'MyFavesDatabase');
 
     if($conn->connect_error) {
-        die('Could not connecto to the database.'.$conn->connect_error);
+        die('Could not connect to the database.'.$conn->connect_error);
     }
 
 ?>
