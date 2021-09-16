d2fad26ce6f0804db5a776c3df70ee51b7a1fb8f
lizmery <l.vigil096@gmail.com>
2021-09-16 13:51:44 -0700

    Update config.php
    Change connection from localhost to heroku

--- config.php
+++ config.php
@@ -1,8 +1,16 @@
 <?php
-    $conn = new mysqli('localhost', 'root', '', 'MyFavesDatabase');
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if($conn->connect_error) {
         die('Could not connecto to the database.'.$conn->connect_error);
     }
-
 ?>
